0af4224fb3c70f6cf7471347208f9a64dc05b980
Jah <kennedye2@vcu.edu>
2021-07-18 16:38:45 +0200

    put deviemotion into its own file

---
+++ scripts/deviceMotion.js
@@ -0,0 +1,9 @@
+export function setDeviceMotion(){
+    var speed = 0.00;
+
+    window.addEventListener('devicemotion', function(event) {
+      speed = parseFloat((speed + event.acceleration.x).toFixed(2));
+      document.getElementById("tracker").innerHTML = speed;
+    });
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,5 @@
+import {setDeviceMotion} from './deviceMotion.js'
+
 document.addEventListener("DOMContentLoaded", event => {
   setDeviceMotion();
 })
-
-function setDeviceMotion(){
-    var speed = 0.00;
-
-    window.addEventListener('devicemotion', function(event) {
-      speed = parseFloat((speed + event.acceleration.x).toFixed(2));
-      document.getElementById("tracker").innerHTML = speed;
-    });
-
-}
